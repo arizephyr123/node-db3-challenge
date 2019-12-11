72c649579735899f4f3408ac863b946f18751ac8
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-11 14:02:22 -0800

    db-config.js created, linked to scheme-router.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,22 @@
+const db = require("../data/db-config.js")
+
+module.exports = {
+find,
+findById,
+findSteps,
+add,
+update,
+remove
+}
+
+function find() {
+db.
+}
+
+
+// findById,
+// findSteps,
+// add,
+// update,
+// remove
+
