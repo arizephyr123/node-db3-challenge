abfea3274d1d8e29123d62f33a99dea6cdeee1d5
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-11 16:39:43 -0800

    MVP completed, schemes CRUD all functional

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,22 +1,67 @@
-const db = require("../data/db-config.js")
+const db = require("../data/db-config.js");
 
 module.exports = {
-find,
-findById,
-findSteps,
-add,
-update,
-remove
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
 
 function find() {
-db.
+  return db("schemes");
 }
 
+async function findById(id) {
+  return await db("schemes")
+    .where({ id })
+    .first()
+    .then(scheme => {
+      if (scheme) {
+        return scheme;
+      } else {
+        return null;
+      }
+    });
+}
 
-// findById,
-// findSteps,
-// add,
-// update,
-// remove
+function findSteps(scheme_id) {
+  return db("steps").where({ scheme_id: scheme_id });
+}
 
+function add(newScheme) {
+  if (!newScheme.scheme_name) {
+    return null;
+  } else {
+    return db("schemes")
+      .insert(newScheme, "id")
+      .then(ids => {
+        const [id] = ids;
+        return findById(id);
+      });
+  }
+}
+
+function update(changes, id) {
+  console.log("in MODEL Update", id, changes);
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  const toDelete = findById(id)
+  .then(removed => removed)
+  return db("schemes")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        return toDelete;
+      }
+    });
+}
